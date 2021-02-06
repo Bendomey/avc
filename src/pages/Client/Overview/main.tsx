f31940dd3de89097164e119d634e6f53e300b94e
Benjamin Domey <domeybenjamin1@gmail.com>
2021-02-06 05:28:49 +0000

    added client overview page

--- src/pages/Client/Overview/main.tsx
+++ src/pages/Client/Overview/main.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 
 const Overview = () => {
-  return <React.Fragment>client overview here</React.Fragment>;
+  return (
+    <React.Fragment>
+      <h1>client overview herehereherehereherehere</h1>
+    </React.Fragment>
+  );
 };
 
 export default Overview;

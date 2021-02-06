be65589c63416aae39c62cde0495d950897dd705
Benjamin Domey <domeybenjamin1@gmail.com>
2021-02-06 05:28:16 +0000

    added global css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./tailwind.css";
 import "./shared/styles/modals.css";
+import "./shared/styles/global.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 

23ae05bdf4b18c713a9d4c3dab2e8679c56c3461
Benjamin Domey <domeybenjamin1@gmail.com>
2021-02-06 05:29:46 +0000

    all green in main layout

--- src/shared/layout/main.tsx
+++ src/shared/layout/main.tsx
@@ -1,35 +1,19 @@
 import * as React from "react";
 import Header from "./header";
-import { Route, Switch } from "react-router-dom";
-import { RouteType as IRoute } from "../interfaces/misc";
-import routes from "../../routes";
 import { PageLoader } from "../../components/atoms/loadingComponents";
-import ClientSettings from "../../pages/Client/Settings";
-import LawyerSettings from "../../pages/Lawyer/Settings";
+import ClientDashbaord from "./client";
+import LawyerDashboard from "./laywer";
 
-let user: "client" | "lawyer" = "lawyer";
+let user: "client" | "lawyer" = "client";
 const Layout = () => {
   return (
     <React.Fragment>
-      <Header />
-      <React.Suspense fallback={PageLoader()}>
-        <Switch>
-          {user === "lawyer" ? (
-            <Route path={"/settings"} exact={true} component={LawyerSettings} />
-          ) : (
-            <Route path={"/settings"} exact={true} component={ClientSettings} />
-          )}
-          {routes?.[user]?.map((route: IRoute, i: number) => (
-            <React.Fragment key={i}>
-              <Route
-                path={route.path}
-                component={route.component as any}
-                exact={route.exact}
-              />
-            </React.Fragment>
-          ))}
-        </Switch>
-      </React.Suspense>
+      <div className="min-h-screen bg-gray-100">
+        <Header />
+        <React.Suspense fallback={PageLoader()}>
+          {user === "client" ? <ClientDashbaord /> : <LawyerDashboard />}
+        </React.Suspense>
+      </div>
     </React.Fragment>
   );
 };

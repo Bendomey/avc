707f269c500109a432ab03c242e98f5b5e3da8d4
Benjamin Domey <domeybenjamin1@gmail.com>
2021-05-19 18:34:33 +0000

    added queries

--- src/services/graphql/queries.ts
+++ src/services/graphql/queries.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 //get countries
 export const GET_COUNTRIES = gql`
-  query($filter: GetCountriesFilter, $skip: Int, $limit: Int) {
+  query ($filter: GetCountriesFilter, $skip: Int, $limit: Int) {
     countries(filter: $filter, pagination: { skip: $skip, limit: $limit }) {
       id
       name
@@ -17,7 +17,7 @@ export const GET_COUNTRIES = gql`
 
 //get tags
 export const GET_TAGS = gql`
-  query($skip: Int, $limit: Int, $filter: GetTagsFilter) {
+  query ($skip: Int, $limit: Int, $filter: GetTagsFilter) {
     tags(pagination: { skip: $skip, limit: $limit }, filter: $filter) {
       id
       name
@@ -29,5 +29,67 @@ export const GET_TAGS = gql`
       updatedAt
     }
     tagsLength(filter: $filter)
+  }
+`;
+
+//get packages
+export const GET_PACKAGES = gql`
+  query ($filter: GetPackagesFilter, $skip: Int, $limit: Int) {
+    packages(filter: $filter, pagination: { skip: $skip, limit: $limit }) {
+      id
+      name
+      amountPerYear
+      amountPerMonth
+      description
+      status
+      createdAt
+      updatedAt
+      requestedBy {
+        id
+        lastName
+      }
+    }
+    packagesLength(filter: $filter)
+  }
+`;
+
+//get package services
+export const GET_PACKAGE_SERVICES = gql`
+  query ($filter: GetPackageServicesFilter, $skip: Int, $limit: Int) {
+    packageServices(
+      filter: $filter
+      pagination: { skip: $skip, limit: $limit }
+    ) {
+      id
+      isActive
+      quantity
+      type
+      package {
+        id
+        name
+      }
+      service {
+        id
+        name
+      }
+      createdAt
+      updatedAt
+    }
+    packageServicesLength(filter: $filter)
+  }
+`;
+
+export const GET_SERVICES = gql`
+  query ($filter: GetServicesFilter, $skip: Int, $limit: Int) {
+    services(filter: $filter, pagination: { skip: $skip, limit: $limit }) {
+      id
+      name
+      description
+      price
+      type
+      createdAt
+      updatedAt
+    }
+    servicesLength(filter: $filter)
   }
 `;

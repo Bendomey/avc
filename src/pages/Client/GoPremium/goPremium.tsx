69c17b3cb713d17d69dbb64033acb2050119b4c2
Benjamin Domey <domeybenjamin1@gmail.com>
2021-05-19 18:33:47 +0000

    fetched packages

--- src/pages/Client/GoPremium/goPremium.tsx
+++ src/pages/Client/GoPremium/goPremium.tsx
@@ -1,52 +1,41 @@
+import { useQuery } from "@apollo/client";
 import * as React from "react";
+import { AuthContext } from "../../../services/context";
+import { GET_PACKAGES } from "../../../services/graphql/queries";
+import {
+  GetPackagesInputProps,
+  GetPackagesOutputProps,
+  Package,
+} from "../../../shared/interfaces/package";
 import { AddPackage } from "./Add";
 import { ApproveSubscription } from "./ApproveSubscription";
 import { EmptyTemplate } from "./Components/emptyTemplate";
 import { SetPeriod } from "./Components/set-period";
 import PackageCard from "./Package";
-
-const tiers = [
-  {
-    name: "Hobby",
-    href: "#",
-    priceMonthly: 12,
-    description: "All the basics for starting a new business",
-    includedFeatures: [
-      "Potenti felis, in cras at at ligula nunc.",
-      "Orci neque eget pellentesque.",
-    ],
-  },
-  {
-    name: "Freelancer",
-    href: "#",
-    priceMonthly: 24,
-    description: "All the basics for starting a new business",
-    includedFeatures: [
-      "Potenti felis, in cras at at ligula nunc. ",
-      "Orci neque eget pellentesque.",
-      "Donec mauris sit in eu tincidunt etiam.",
-    ],
-  },
-  {
-    name: "Startup",
-    href: "#",
-    priceMonthly: 32,
-    description: "All the basics for starting a new business",
-    includedFeatures: [
-      "Potenti felis, in cras at at ligula nunc. ",
-      "Orci neque eget pellentesque.",
-      "Donec mauris sit in eu tincidunt etiam.",
-      "Faucibus volutpat magna.",
-    ],
-  },
-];
+import PuffLoader from "react-spinners/PuffLoader";
+import {
+  EmptyAlertComponent,
+  ErrorAlertComponent,
+} from "../../../components/atoms/alertComponents";
 
 export default function GoPremium() {
+  const [, state] = React.useContext(AuthContext);
   const [period, setPeriod] = React.useState<"monthly" | "yearly">("monthly");
   const [add, setAdd] = React.useState(false);
 
   const [approve, setApprove] = React.useState(false);
   const [selected, setSelected] = React.useState<any>(null);
+
+  const { data, loading } = useQuery<
+    GetPackagesOutputProps,
+    GetPackagesInputProps
+  >(GET_PACKAGES, {
+    variables: {
+      filter: {
+        user: state?.userToken?.user?.id,
+      },
+    },
+  });
 
   return (
     <React.Fragment>
@@ -61,27 +50,54 @@ export default function GoPremium() {
               your assigned lawyer to complete all tasks according to an agreed
               schedule
             </p>
-            <SetPeriod period={period} setPeriod={setPeriod} />
-          </div>
-          <div className="relative mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-4">
-            {tiers.map((tier: any, i: number) => (
-              <React.Fragment key={i}>
-                <PackageCard
-                  onChoose={() => {
-                    setSelected(tier);
-                    setApprove(true);
-                  }}
-                  tier={tier}
-                  period={period}
-                />
-              </React.Fragment>
-            ))}
-            {add ? (
-              <AddPackage setAdd={setAdd} />
-            ) : (
-              <EmptyTemplate setAdd={setAdd} />
+            {data && data?.packagesLength > 0 && (
+              <SetPeriod period={period} setPeriod={setPeriod} />
             )}
           </div>
+
+          {loading ? (
+            <React.Fragment>
+              <div className={"h-56 flex justify-center items-center"}>
+                <PuffLoader color={"red"} size={50} />
+              </div>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              {data ? (
+                <React.Fragment>
+                  {data?.packagesLength === 0 ? (
+                    <EmptyAlertComponent model={"packages"} />
+                  ) : (
+                    <React.Fragment>
+                      <div className="relative mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-4">
+                        {data?.packages.map((tier: Package, i: number) => (
+                          <React.Fragment key={i}>
+                            <PackageCard
+                              onChoose={() => {
+                                setSelected(tier);
+                                setApprove(true);
+                              }}
+                              tier={tier}
+                              period={period}
+                            />
+                          </React.Fragment>
+                        ))}
+                        {add ? (
+                          <AddPackage setAdd={setAdd} />
+                        ) : (
+                          <EmptyTemplate setAdd={setAdd} />
+                        )}
+                      </div>
+                    </React.Fragment>
+                  )}
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  <ErrorAlertComponent model={"packages"} />
+                </React.Fragment>
+              )}
+            </React.Fragment>
+          )}
         </div>
       </div>
       <ApproveSubscription
